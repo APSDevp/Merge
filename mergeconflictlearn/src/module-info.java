76705d33c4fcdf1708e31c3bf428296b996d11cd
dhinesh <dhineshpazanisamy@gmail.com>
2022-03-03 23:08:38 +0530

    commit only v1

--- mergeconflictlearn/src/module-info.java
+++ mergeconflictlearn/src/module-info.java
@@ -5,5 +5,6 @@ module mergeconflictlearn {
 	//new lines is added aginst dev line in local
 	//learning conflict resolution v1
 	//learning conflict v2 git edit
+	//only doing commit not push v1
 
 }
